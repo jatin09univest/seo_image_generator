c52bf980c4fd402880747f62b0adf9cfc63b221e
Shiva Singh <shivasingh.200087@gmail.com>
2026-04-02 22:43:10 +0530

    Add environment configuration for API tokens and URLs

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    UNIVEST_API_TOKEN:
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIyMTgiLCJpYXQiOjE1MTYyMzkwMjJ9.oab07p4mUDggobg3w7rm3Doc1ZS9VV0eVl5rrRf4lC8",
+    UNIVEST_API_URL: "https://uat-api.univest.in/api/utility/generate-image",
+    UNIVEST_BULK_API_URL:
+      "https://uat-api.univest.in/api/utility/generate-images",
+    GOOGLE_SERVICE_ACCOUNT_JSON: "",
+    GOOGLE_DRIVE_FOLDER_ID: "",
+  },
 };
 
 export default nextConfig;
